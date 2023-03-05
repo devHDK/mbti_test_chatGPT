2a5ba85446dd6d35bb663a5bbdabb84d602d9607
devHDK <hyundo91082@gmail.com>
2023-03-05 15:06:19 +0900

    여행지로 바꿈

--- src/MBTI_test.js
+++ src/MBTI_test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 
 const OPENAI_API_KEY = ''
 const OPENAI_MODEL = 'text-davinci-002'
@@ -8,10 +8,18 @@ function MBTI_test() {
     const [personalityInput, setPersonalityInput] = useState('')
     const [result, setResult] = useState('')
     const [mbti, setMbti] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+
+        console.log(loading)
+
+    },[loading])
 
     async function handleMbtiButtonClick() {
         // 성격묘사를 ChatGPT API에 전송하여 MBTI 유형 예측
         try {
+            setLoading(true)
             const mbtiApiResponse = await fetch('https://api.openai.com/v1/completions', {
                 method: 'POST',
                 headers: {
@@ -20,9 +28,9 @@ function MBTI_test() {
                 },
                 body: JSON.stringify(
                     {
-                        prompt: `Please provide the personality description described later in MBTI code with description as JSON Object like {"code":" ","desc":" "} 
+                        prompt: `If I explain where I want to go, tell me the detailed description and the city in the form of {"city":", "desc":"} in JSON format. 
                                  \n\n
-                                 personality : ${personalityInput} 
+                                 explain : ${personalityInput} 
                                  `,
 
                         model: OPENAI_MODEL,
@@ -31,12 +39,14 @@ function MBTI_test() {
                 ),
             })
             const mbtiApiResponseJson = await mbtiApiResponse.json()
+            console.log(JSON.parse(mbtiApiResponseJson.choices[0].text))
 
             // 예측된 MBTI 코드에 해당하는 결과와 설명
-            const { code, desc, } = JSON.parse(mbtiApiResponseJson.choices[0].text)
-            if (desc) {
-                setMbti(code)
+            const { city, desc, } = JSON.parse(mbtiApiResponseJson.choices[0].text)
+            if (city) {
+                setMbti(city)
                 setResult(`${desc}`)
+                setLoading(false)
             } else {
                 setMbti('?')
                 setResult('알 수 없는 유형입니다 😕')
@@ -44,15 +54,17 @@ function MBTI_test() {
 
         } catch (e) {
             console.log(e)
+            setLoading(false)
+            window.alert('error')
         }
 
     }
 
     return (
         <div style={{ marginLeft: '20px' }}>
-            <h1>MBTI 유형 예측기</h1>
+            <h1>가고싶은 여행지 추천</h1>
             <div>
-                <label htmlFor="personalityInput">성격 : </label>
+                <label>가고싶은곳 : </label>
                 <input
                     style={{ width: "1000px", }}
                     type="text"
@@ -62,7 +74,7 @@ function MBTI_test() {
                 />
             </div>
             <div>
-                <button style={{ margin: '10px' }} onClick={handleMbtiButtonClick}>MBTI 확인</button>
+                <button style={{ margin: '10px' }} onClick={handleMbtiButtonClick}> {!loading ? '확인' : <span style={{color:'gray'}} > loading... </span>}</button>
             </div>
             <div style={{ width: '800px', height: '800px', overflow: 'auto', margin: '10px' }}>
                 {mbti && <div style={{ fontSize: '30px', marginBottom: '10px' }} > {mbti}</div>}
